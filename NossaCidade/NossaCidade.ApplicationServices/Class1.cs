caec0024c4817f8a5fdee553b2c66105451b2bd4
paulo-ortins <paulo.ortins@gmail.com>
2012-08-26 13:55:07 -0300

    Voltando versao

---
+++ NossaCidade/NossaCidade.ApplicationServices/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ApplicationServices
+{
+    public class Class1
+    {
+         tete
+    }
+}
